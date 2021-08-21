fa3d829f875560325ba1cf551936a92890effea3
Dominik <dominik.stefanik@gmail.com>
2021-08-21 17:19:17 +0100

    started Front End, added Bootstrap

--- src/main/java/com/dominiks/demo/controller/HomeController.java
+++ src/main/java/com/dominiks/demo/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.dominiks.demo.controller;
 
+import com.dominiks.demo.response.MarsRoverApiResponse;
+import com.dominiks.demo.service.MarsRoverApiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private MarsRoverApiService roverApiService;
+
     @GetMapping("/")
     public String getHomeView(Model model) {
-        model.addAttribute("nameAndSurnameInitial", "Dominik S");
+        MarsRoverApiResponse roverData = roverApiService.getRoverData();
+        model.addAttribute("roverData", roverData);
         return "index";
     }
 }
